005656a6fa29eadf2b8cec9f7d8993153264c4e6
Joselyn_Shmaeff <jbshmaeff@yahoo.com>
2021-10-29 16:06:24 -0700

    Added images, display image code, and button click

--- js/script.js
+++ js/script.js
@@ -6,6 +6,32 @@ const img = document.querySelector(".img");
 const getImage = async function () {
     const res = await fetch ("https://picsum.photos/v2/list?limit=100");
     const images = await res.json();
-    console.log(images);
+    //console.log(images);
+    selectRandomImage(images);
 };
-getImage();
+
+
+//Randomly Select the Index of an Image
+const selectRandomImage = function (images) {
+    const randomIndex = Math.floor(Math.random()*images.length);
+    //console.log(randomIndex);
+    const randomImage = images[randomIndex];
+    //console.log(randomImage);
+    displayImage(randomImage);
+};
+
+//Display the image
+
+const displayImage = function (randomImage) {
+    const author = randomImage.author;
+    const imageAddress = randomImage.download_url;
+    authorSpan.innerText = author;
+    img.src= imageAddress;
+    imgDiv.classList.remove ("hide");
+};
+
+//Add a Click Event & Commit Changes
+
+button.addEventListener("click", function () {
+    getImage();
+});
